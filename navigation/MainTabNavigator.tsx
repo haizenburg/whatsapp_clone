74c944fe97a12e1a68dfe8ab5cd75b592c0e0474
haizenburg <tshepangmoima87@gmail.com>
2022-08-31 14:56:02 +0200

    main header navigation

---
+++ navigation/MainTabNavigator.tsx
@@ -0,0 +1,57 @@
+import { View, Text, useColorScheme, Pressable } from "react-native";
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import { MainTabParamList, RootTabScreenProps } from "../types";
+import { FontAwesome } from "@expo/vector-icons";
+import TabOneScreen from "../screens/TabOneScreen";
+import TabTwoScreen from "../screens/TabTwoScreen";
+import Colors from "../constants/Colors";
+
+import { Fontisto } from "@expo/vector-icons";
+
+const MainTab = createMaterialTopTabNavigator<MainTabParamList>();
+
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+const MainTabNavigator = () => {
+  const colorScheme = useColorScheme();
+  return (
+    <MainTab.Navigator
+      initialRouteName="Chats"
+      screenOptions={{
+        tabBarActiveTintColor: Colors.light.background,
+        tabBarStyle: {
+          backgroundColor: Colors.light.tint,
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: Colors.light.background,
+          height: 3,
+        },
+        tabBarLabelStyle: { fontWeight: "bold" },
+        tabBarShowIcon: true,
+      }}
+    >
+      <MainTab.Screen
+        name="Camera"
+        component={TabTwoScreen}
+        options={({ navigation }: RootTabScreenProps<"Camera">) => ({
+          tabBarIcon: ({ color }) => (
+            <Fontisto name="camera" color={color} size={18} />
+          ),
+          tabBarLabel: () => null,
+        })}
+      />
+      <MainTab.Screen name="Chats" component={TabTwoScreen} />
+      <MainTab.Screen name="Status" component={TabTwoScreen} />
+      <MainTab.Screen name="Calls" component={TabTwoScreen} />
+    </MainTab.Navigator>
+  );
+};
+
+export default MainTabNavigator;
